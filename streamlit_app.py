7933708b018917d52c37af2863acc722c32e5719
Alei_Khorshed <aleikhorshed@outlook.com>
2026-01-12 19:43:33 +0000

    update 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
-import streamlit as st
+import streamlit as st 
 
 st.title("🎈 Alei Khorshed - Planning App")
 
 st.write("My IBDP Computer Science - IA")
 
 # st.write("For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
+
+st.write("hello")
+
