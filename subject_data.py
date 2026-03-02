4c6d1fbcc93dd49263af09b7deb743dfe14373e7
Alei_Khorshed <aleikhorshed@outlook.com>
2026-03-02 21:00:23 +0000

    Tasks Dataframe

--- subject_data.py
+++ subject_data.py
@@ -19,7 +19,7 @@ conn = sql.connect(MyDB)
 
 # Read the entire table into a DataFrame
 # Create a SQL command to read data from the table 
-df = pd.read_sql("SELECT * FROM Subject", conn)
+df = pd.read_sql("SELECT title FROM Subject", conn)
 # Display the dataframe on the page
 st.write(df)
 
