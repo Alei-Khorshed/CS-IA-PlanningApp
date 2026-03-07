b75eb5ba347e923ac8744ab7d2ac6fdb822acc0f
Alei_Khorshed <aleikhorshed@outlook.com>
2026-03-07 14:24:12 +0000

    Updates

--- subject_data.py
+++ subject_data.py
@@ -5,80 +5,80 @@ import sqlite3 as sql     # sqllite3 library to work with a sqllite database
 
 
 try:
+ 
+    i = 1/0
 
-    i= 10/0
-    
+
+
+    # Display the page title at the top of the page and in the left navigation sidebar
+    st.markdown("# Subject Data")
+    st.sidebar.markdown("# Subject Data")
+
+
+    def display_subject_form():
+        # Set the variable name for my DB
+        MyDB = "CS IA DB.db"
+
+        # Create DB Connection 
+        conn = sql.connect(MyDB)
+
+        # Read the entire table into a DataFrame
+        # Create a SQL command to read data from the table 
+        df = pd.read_sql("SELECT title FROM Subject", conn)
+        # Display the dataframe on the page
+        st.dataframe(df , hide_index=True)
+
+        # Create a form to enter data
+        with st.form("data_form", clear_on_submit=True):
+            title = st.text_input("Subject Title", key="txtTitle")
+            # Add a sumbit button
+            submit = st.form_submit_button("Add Subject")
+
+            # Save the new record into the database
+            if submit:
+                # Create a DataFrame for the new record
+                #data_record = [{"subject_id": id, "title": title}]
+                data_record = [{"subject_id": 0, "title": title}]
+                df_data = pd.DataFrame(data_record)
+                
+                # Write to SQLite
+                # 'append' adds to the table; 'replace' would overwrite it        
+                #df_data.to_sql("Subject", conn, if_exists="append", index=False)
+                
+
+                # Create a SQL command to save the record to the database
+                cur = conn.cursor()
+                cur.executemany("INSERT INTO Subject VALUES(NULL, :title)", data_record)
+                # Commit Database to save changes
+                conn.commit() 
+
+                st.session_state.first_load = "NO"
+
+                # Close Database connection           
+                conn.close()
+                st.rerun()
+
+        return
+
+
+
+
+
+    # Add a button to navigate back to the home page        
+    if st.button("Save and Exit"):
+        st.switch_page("home.py")
+
+
+    # Button to delete and reset all records
+    if st.button("DELETE ALL Subjects"):
+        # Delete all records
+        cur = conn.cursor()
+        cur.execute("DELETE FROM Subject ")    
+        conn.commit() 
+        conn.close()
+        st.rerun()
+
 
 except Exception as err:
     print(f"Unexpected {err=}, {type(err)=}")
-    
-
-
-
-# Display the page title at the top of the page and in the left navigation sidebar
-st.markdown("# Subject Data")
-st.sidebar.markdown("# Subject Data")
-
-
-def display_subject_form():
-    # Set the variable name for my DB
-    MyDB = "CS IA DB.db"
-
-    # Create DB Connection 
-    conn = sql.connect(MyDB)
-
-    # Read the entire table into a DataFrame
-    # Create a SQL command to read data from the table 
-    df = pd.read_sql("SELECT title FROM Subject", conn)
-    # Display the dataframe on the page
-    st.dataframe(df , hide_index=True)
-
-    # Create a form to enter data
-    with st.form("data_form", clear_on_submit=True):
-        title = st.text_input("Subject Title", key="txtTitle")
-        # Add a sumbit button
-        submit = st.form_submit_button("Add Subject")
-
-        # Save the new record into the database
-        if submit:
-            # Create a DataFrame for the new record
-            #data_record = [{"subject_id": id, "title": title}]
-            data_record = [{"subject_id": 0, "title": title}]
-            df_data = pd.DataFrame(data_record)
-            
-            # Write to SQLite
-            # 'append' adds to the table; 'replace' would overwrite it        
-            #df_data.to_sql("Subject", conn, if_exists="append", index=False)
-            
-
-            # Create a SQL command to save the record to the database
-            cur = conn.cursor()
-            cur.executemany("INSERT INTO Subject VALUES(NULL, :title)", data_record)
-            # Commit Database to save changes
-            conn.commit() 
-
-            st.session_state.first_load = "NO"
-
-            # Close Database connection           
-            conn.close()
-            st.rerun()
-
-    return
-
-
-
-
-
-# Add a button to navigate back to the home page        
-if st.button("Save and Exit"):
-    st.switch_page("home.py")
-
-
-# Button to delete and reset all records
-if st.button("DELETE ALL Subjects"):
-    # Delete all records
-    cur = conn.cursor()
-    cur.execute("DELETE FROM Subject ")    
-    conn.commit() 
-    conn.close()
-    st.rerun()
+    raise
